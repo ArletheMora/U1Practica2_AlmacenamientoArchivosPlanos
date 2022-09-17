9293aab659085e67c67f506342009ad3e0e97013
heleperezcr <heleperezcr@ittepic.edu.mx>
2022-09-17 18:52:49 -0500

    fragment registro y barra lateral

--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientoarchivosplanos/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientoarchivosplanos/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //val citas = Array(20)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
