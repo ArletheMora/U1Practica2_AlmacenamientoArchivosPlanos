ccb6352264325807a8bbf6b32b6b87c34ba78d0e
heleperezcr <heleperezcr@ittepic.edu.mx>
2022-09-22 18:06:17 -0500

    funciones de agregar buscar actualizar y eliminar con el archivo de texto

--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientoarchivosplanos/ui/home/HomeFragment.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientoarchivosplanos/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package mx.edu.ittepic.ladm_u1_practica2_almacenamientoarchivosplanos.ui.home
 
+import android.R
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
@@ -8,6 +10,7 @@ import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -31,10 +34,9 @@ class HomeFragment : Fragment() {
 
 
     //Arreglo para datos------------------
-    val nombres = arrayOfNulls<String>(21)
-    val edades = arrayOfNulls<String>(21)
-    val descripciones = arrayOfNulls<String>(21)
-    var registros = arrayOfNulls<String>(21)
+    var listaRegistros = ArrayList<String>()
+    var posicionActualizar = -1
+
 
     var cad1: String = ""
     var cad2: String = ""
@@ -60,21 +62,18 @@ class HomeFragment : Fragment() {
 
         /*HACER LECTURA ANTES */
         binding.agregar.setOnClickListener {
-            cad1 = binding.txtnombre.text.toString()
-            cad2 = binding.txtedad.text.toString()
-            cad3 = binding.txtdescripcion.text.toString()
-            cad4 = binding.txtnocita.text.toString()
-            nombres[cont]=cad1
-            edades[cont]=cad2
-            descripciones[cont]=cad3
-            registros[cont]= cad1 +"\n" + cad2 +"\n"+ cad3
-            view?.let { it1 ->
-                Snackbar.make(it1, "Se agrego correctamente \n Su numero de cita es el: "+cont, Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-            }
+            insertar()
+            guardarEnArchivo()
             borrar()
 
-            guardarEnArchivo( cad1 + "|" + cad2 + "|" + cad3 + "|" + cad4   )
+            view?.let { it1 ->
+                Snackbar.make(
+                    it1,
+                    "Se agrego correctamente \n Su numero de cita es el: " + cont,
+                    Snackbar.LENGTH_LONG
+                )
+                    .setAction("Action", null).show()
+            }
 
             cont=cont+1
         }
@@ -82,62 +81,51 @@ class HomeFragment : Fragment() {
 
 
         binding.buscar.setOnClickListener {
-            try{
-                if(binding.txtnocita.equals("")){
-                    view?.let { it1 ->
-                        Snackbar.make(it1, "No se ingreso No. de Cita", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show()
-                    }
-                }else if(nombres[Integer.parseInt(binding.txtnocita.text.toString())].equals("") || edades[Integer.parseInt(binding.txtnocita.text.toString())].equals("") || descripciones[Integer.parseInt(binding.txtnocita.text.toString())].equals("")){
-                    view?.let { it1 ->
-                        Snackbar.make(it1, "No se encontro el registro", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show()
-                    }
-                }else{
-                    binding.txtnombre.setText(nombres[Integer.parseInt(binding.txtnocita.text.toString())])
-                    binding.txtedad.setText(edades[Integer.parseInt(binding.txtnocita.text.toString())])
-                    binding.txtdescripcion.setText(descripciones[Integer.parseInt(binding.txtnocita.text.toString())])
-                }
-            }catch (e: Exception){
-                Toast.makeText(context, "No se ingresó No. de Cita", Toast.LENGTH_LONG)
-                    .show()
-            }
+
+            guardarDesdeArchivo()
+            posicionActualizar = Integer.parseInt(binding.txtnocita.text.toString())
+            var temporal =
+                listaRegistros.get(Integer.parseInt(binding.txtnocita.text.toString()))
+                    .split("\n")
+            binding.txtnombre.setText(temporal[0])
+            binding.txtedad.setText(temporal[1])
+            binding.txtdescripcion.setText(temporal[2])
 
 
         }
 
         binding.actualizar.setOnClickListener {
-
-            try {
-                cad1 = binding.txtnombre.text.toString()
-                cad2 = binding.txtedad.text.toString()
-                cad3 = binding.txtdescripcion.text.toString()
-                nombres[Integer.parseInt(binding.txtnocita.text.toString())]=cad1
-                edades[Integer.parseInt(binding.txtnocita.text.toString())]=cad2
-                descripciones[Integer.parseInt(binding.txtnocita.text.toString())]=cad3
-                view?.let { it1 ->
-                    Snackbar.make(it1, "Se actualizo el registro", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
+            posicionActualizar = Integer.parseInt(binding.txtnocita.text.toString())
+            if(posicionActualizar ==-1){//SI estamos en -1 no hemos activado la opcion de actualizacion
+                context?.let { it1 ->
+                    AlertDialog.Builder(it1)
+                        .setTitle("ERROR")
+                        .setMessage("NO SELECCIONO REGISTRO A ACTUALIZAR")
+                        .show()
                 }
-
-                borrar()
-            }catch (e: Exception){
-            Toast.makeText(context, "No se ingresó No. de Cita", Toast.LENGTH_LONG)
-                .show()
-        }
-
+                //return@setOnClickListener
+            }
+            var concatenacioon =
+                binding.txtnombre.text.toString() + "\n" + binding.txtedad.text.toString() + "\n" + binding.txtdescripcion.text.toString()
+            listaRegistros.set(posicionActualizar, concatenacioon)
+            binding.listacontactos.adapter = ArrayAdapter<String>(
+                binding.root.context,
+                R.layout.simple_list_item_1, listaRegistros
+            )
+            //binding.txtnombre.setText("")
+            //binding.txtedad.setText("")
+            //binding.txtdescripcion.setText("")
+            posicionActualizar = -1
+            guardarEnArchivo()
+            borrar()
 
         }
 
         binding.eliminar.setOnClickListener {
-            nombres[Integer.parseInt(binding.txtnocita.text.toString())]=""
-            edades[Integer.parseInt(binding.txtnocita.text.toString())]=""
-            descripciones[Integer.parseInt(binding.txtnocita.text.toString())]=""
+
+            listaRegistros.removeAt(Integer.parseInt(binding.txtnocita.text.toString()))
+            guardarEnArchivo()
             borrar()
-            view?.let { it1 ->
-                Snackbar.make(it1, "Se elimino el registro", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-            }
 
         }
 
@@ -151,46 +139,90 @@ class HomeFragment : Fragment() {
         binding.txtnocita.setText("")
     }
 
-    fun guardarEnArchivo(mensaje:String){
+    //------------------------------------------------------------
+    private fun guardarDesdeArchivo() {
         try {
-            var fileContents = ""
+            var archivo = BufferedReader(InputStreamReader(
+                binding.root.context.openFileInput("datos.txt")
+            ))
 
-            //construir archivo
-            var archivo = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString() + "/" + "datosCitas.txt"
+            var bufferContenido = ""
 
-            var file = File(archivo)
+            //va a ir leyendo línea por línea
+            var interactivo = archivo.lineSequence().iterator()
 
-            if (!file.exists()) {       //si no existe se crea
-                file.createNewFile()
-            }else{                      //si existe que actualice
-
-                val file1 = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
-                    .toString() + "/datosCitas.txt")
-
-                FileReader(file1).use {
-                    val chars = CharArray(file1.length().toInt())
-                    it.read(chars)
-                    val fileContent = String(chars)
-                    fileContents = fileContent.toString()
-                }
+            while(interactivo.hasNext()) { //regresa un booleano si hay línea
+                bufferContenido += interactivo.next()+"\n"
             }
 
-            val fileWriter = FileWriter(file)
-            val bufferedWriter = BufferedWriter(fileWriter)
-            if (fileContents != ""){
-                bufferedWriter.write(fileContents + "-" + mensaje)
-            }else{
-                bufferedWriter.write(mensaje)
-            }
-            bufferedWriter.close()
+            //vector para tener la  lista de contactos
+            var vector = bufferContenido.split("&&")
+            listaRegistros.clear()
 
-            Toast.makeText(context, "GUARDADO", Toast.LENGTH_LONG)
-                .show()
+            for (v in vector){
+                listaRegistros.add(v)
+
+            }
+            binding.listacontactos.adapter =  ArrayAdapter<String>(binding.root.context,
+                android.R.layout.simple_list_item_1, listaRegistros)
+
         }catch (e:Exception){
-            Toast.makeText(context, e.message, Toast.LENGTH_LONG)
+            context?.let {
+                AlertDialog.Builder(it)
+                    .setTitle("Error")
+                    .setMessage(e.message)
+                    .setPositiveButton("OK"){d, i->}
+                    .show()
+            }
+        }
+
+    }
+    //-------------------------------------------------------------
+
+    private fun insertar(){
+
+        var concatenacion = binding.txtnombre.text.toString() + "\n" + binding.txtedad.text.toString()+ "\n" + binding.txtdescripcion.text.toString()
+        listaRegistros.add(concatenacion)
+
+        //mostrar en lista                                      //this                  //conjunto de items                 //datos
+        binding.listacontactos.adapter = ArrayAdapter<String>(binding.root.context, android.R.layout.simple_list_item_1, listaRegistros)
+
+
+        binding.listacontactos.adapter = ArrayAdapter<String>(binding.root.context, android.R.layout.simple_list_item_1, listaRegistros)
+    }
+
+    //-----------------------------------------------------------------
+    private  fun guardarEnArchivo(){
+
+        try {
+            var archivo = OutputStreamWriter(binding.root.context.openFileOutput("datos.txt", Activity.MODE_PRIVATE))
+
+            var bufferContenido = ""
+
+            for (dato in listaRegistros){
+                bufferContenido += dato + "&&"
+            }
+            //el substring para evitar error
+            bufferContenido = bufferContenido.substring(0, bufferContenido.lastIndexOf("&&"))
+            archivo.write(bufferContenido)
+            archivo.flush()
+            archivo.close()
+
+            Toast.makeText(context, "Se guardó correctamente", Toast.LENGTH_LONG)
                 .show()
+
+        }catch (e:Exception){
+            context?.let {
+                AlertDialog.Builder(it)
+                    .setTitle("Error")
+                    .setMessage(e.message)
+                    .setPositiveButton("OK"){d, i->}
+                    .show()
+            }
         }
     }
+
+    //-----------------------------------------------------
 
 
 
